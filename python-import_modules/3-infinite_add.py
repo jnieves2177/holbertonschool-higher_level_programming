20efe98a5a8a77c76ffb13e24994cfec5c60953d
jnieves2177 <jnieves3177@gmail.com>
2023-09-29 12:43:25 +0000

    infinite

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    result = 0
+    for a in range(len(sys.argv) - 1):
+        result += int(sys.argv[a + 1])
+    print("{}".format(result))
