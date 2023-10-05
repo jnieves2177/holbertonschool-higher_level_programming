3bcda3d861ee871486485921630dfe49af4fdcbd
jnieves2177 <jnieves3177@gmail.com>
2023-10-05 16:46:00 +0000

    7 update

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        a_dictionary[key] = value
+    a_dictionary.setdefault(key, value)
+    return (a_dictionary)
