666bed36d01ef5183a21db3e3873ee1ba403c9f6
jnieves2177 <jnieves3177@gmail.com>
2023-10-06 04:47:36 +0000

    0 safe

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for element in my_list:
+            if count < x:
+                print(element, end=' ')
+                count += 1
+        print()
+        return count
+    except TypeError:
+        print("An error occurred. Please provide a valid list.")
+        return 0
