821f63798a6a7a85508fbbe947eb320a9f28c5a5
jnieves2177 <jnieves3177@gmail.com>
2023-10-06 16:19:02 +0000

    3 safe

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        division_result = a / b
+    except:
+        division_result = None
+    finally:
+        print("Inside result: {}".format(division_result))
+    return division_result
