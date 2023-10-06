8cb7ee86c682890f2e53e6d3953aa5c93be10d10
jnieves2177 <jnieves3177@gmail.com>
2023-10-06 16:22:50 +0000

    3 safe

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         division_result = a / b
-    except(TypeError, ZeroDivisionError):
+    except (TypeError, ZeroDivisionError):
         division_result = None
     finally:
         print("Inside result: {}".format(division_result))
