3c469a614218650e399c73c6ed8fdb9d8e725ef0
jnieves2177 <jnieves3177@gmail.com>
2023-10-10 15:22:34 +0000

    3 square

---
+++ python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Defines a class Square.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square.
+        Args:
+        size: The size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square and returns it.
+        """
+        return (self.__size ** 2)
