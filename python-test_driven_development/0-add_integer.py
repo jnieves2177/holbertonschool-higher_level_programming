258f8bf063505b4745517c1a5b0950166c23f7e0
jnieves2177 <jnieves3177@gmail.com>
2023-10-14 02:29:51 +0000

    0 add

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""
+0-add_integer.py Module
+Has one method that returns an int sum
+Accepts two values, whether int or float, and casts them to int before adding
+"""
+
+
 def add_integer(a, b=98):
-    """ Check if a and b are either integers or floats"""
-    if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
-        raise TypeError("a must be an integer or b must be an integer")
-
-    # Cast a and b to integers if they are floats
-    a = int(a) if isinstance(a, float) else a
-    b = int(b) if isinstance(b, float) else b
-
-    # Perform the addition and return the result as an integer
-    return int(a + b)
+    """
+    Returns a + b as int
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
