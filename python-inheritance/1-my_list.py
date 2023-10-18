f52f11629be483f9c329be070ac93e7446053723
jnieves2177 <jnieves3177@gmail.com>
2023-10-18 17:00:18 +0000

    my list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+1-my_list.py
+
+Contains class MyList
+inherits from list; has public instance method to print sorted
+"""
+
+
+class MyList(list):
+    """inherits from list
+
+    methods:
+    print_sorted(self)
+    """
+    def print_sorted(self):
+        """prints list of ints all sorted in ascending order"""
+        print(sorted(self))
